47969bbf85bbdcc9c05284fceccd81023cd4364a
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-05-18 06:52:20 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,23 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "27402174"
+API_ID = "24313439"
 # -------------------------------------------------------------
-API_HASH = "53cdb3d648bf50e7625386b3e6879c23"
+API_HASH = "a007ea183e9276803caba61ad093273b"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("7882598019:AAF9FW-MiuOZi-VJK41YOjBg44Nrnnnxyw4", None)
-STRING1 = getenv("BQGiH74AMnVmD7Z8p4bdNjNjH0dDq0QBLvwYKMP0qkiIvwjeaBL0sFBVLnGdeTsVJ5ceI00NBOFOSiQEJZTBfVP7iIduh5LPUwQ6XupuaslRZ1H0pC_E2VvbctP27g4ArQ_M4RNA99YBesgCmlg11NpOqVIE8LY3HawAmzKQUbiKtJzRyMfhGUv_cJDnc9dJSL1E8kFn_4Pi0IbEaExKXDU2wxbGZJWtWPr4wQH8-hM-CnN9b1OqBcPYuEQpZNhY2hIOKr8L4IJiOA5eaVhQFqHYWK6Su1MiGQXDEQS85xgB4HQp-hQYeGMqqKdAptr2Nv5iCIVR7_Vofp30SdqCKh1Fm8WD0wAAAAG_nNwGAA", None)
-DB_NAME = "shizuDB"
-MONGO_URL = getenv("mongodb+srv://sr7blackbirds:5mRE2CGlkCXmI5pL@cluster0.jsssmx8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+BOT_TOKEN = getenv("7718680866:AAEX_SfFMaE6efdwoGlFjxQnFVVNYV-Yx1g", None)
+STRING1 = getenv("BQFy_l8ATqoLGnvyauM5yWnNjDX1k8J9HUQmEmyczAKGRVg7BJmEWO0qLYQ25At1ycxywKV00iV1DELrvN76bqlygOInqxuQwyC4th4eZJ7MWDK9LosQjkBpZlakXPa2N9uCBsw58pqy-sz4CGt9qMcMZRXy18soKCV-2x6kLvo_La783CckOYVx1iIuQPNyxai0U-NX-lYtXC24ys6CVwHjTIrvUlfHEqWFuJhYBJ80MqnDrAj9DaFUzshzHzuW0Po_y5FJIbyJ5AIBw6EEnVLO6JIm5hA_yZGM4cx3RDXcegEcQb4yAX2ZFeU9GC0WyXmXrIolIZgr5PyWdwQXyjnM2hB7bAAAAAGyiepSAA", None)
+DB_NAME = "Botbaby"
+MONGO_URL = getenv("mongodb+srv://botbaby:botbaby@cluster0.zngip1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 OWNER_ID = int(getenv("OWNER_ID", "1716902346"))
-BOT_ID = int(getenv("BOT_ID", "7882598019"))
-SUPPORT_GRP = "bindaas_buddies"
-UPDATE_CHNL = "SR7_BLACKBIRD"
-OWNER_USERNAME = "UrStaRk"
+BOT_ID = int(getenv("BOT_ID", "7290350162"))
+SUPPORT_GRP = "infinitygx_bot_support"
+UPDATE_CHNL = "gxinfinity_support"
+OWNER_USERNAME = "Its_tg_seller"
 TIME_ZONE = "Asia/Kolkata"
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002540021894"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002620121753"))
 # --------------------------------------------------------------
-SUDOERS = list(map(int, getenv("SUDOERS", "1716902346").split()))
+SUDOERS = list(map(int, getenv("SUDOERS", "7290350162").split()))
 # --------------------------------------------------------------
 
 ### DONT TOUCH or EDIT codes after this line
@@ -30,6 +30,6 @@ BANNED_USERS = filters.user()
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Badhacker98/ShizuChat_Bot",
+    "https://github.com/SpyGamer7t2r/ShizuChat_Bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
